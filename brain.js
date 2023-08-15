9411a22289eff4f048e4ed847f8f5b5be7c1bb56
Justin Forseth <jforseth210@gmail.com>
2023-08-14 23:33:31 -0600

    Start work on "the algorithm" kinda

---
+++ brain.js
@@ -0,0 +1,134 @@
+groups = [
+  {
+    name: "Mon Prayer",
+    days: ["Mon"],
+    counselors: ["Lily K"],
+    minCounselors: 1,
+  },
+  {
+    name: "Tues Prayer",
+    days: ["Tue"],
+    counselors: ["Justin"],
+    minCounselors: 1,
+  },
+  {
+    name: "Wed Prayer",
+    days: ["Wed"],
+    counselors: ["Kara"],
+    minCounselors: 1,
+  },
+  {
+    name: "Thurs Prayer",
+    days: ["Thurs"],
+    counselors: ["Luke"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 1",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Mason", "Grace H"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 2",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Kyley", "Asher"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 3",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Ryan", "Lily T"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 4",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Jack S", "Kara"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 5",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Jack H", "Becca"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 6",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Luke", "Grace K"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 7",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Jack M", "Lily K"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 8",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Mike", "Vanessa"],
+    minCounselors: 1,
+  },
+  {
+    name: "SG 9",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Justin", "Amy"],
+    minCounselors: 1,
+  },
+  {
+    name: "Bear",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Kara", "Kyley", "Amy", "Becca", "Grace H"],
+    minCounselors: 2,
+  },
+  {
+    name: "Wildcat",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Grace K", "Vanessa", "Lily K", "Lily T"],
+    minCounselors: 2,
+  },
+  {
+    name: "Coyote",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Mason", "Jack S", "Mike"],
+    minCounselors: 2,
+  },
+  {
+    name: "Goat's Nest",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Asher", "Jack H", "Justin"],
+    minCounselors: 2,
+  },
+  {
+    name: "Chipmunk",
+    days: ["Mon", "Tue", "Wed", "Thur"],
+    counselors: ["Ryan", "Luke", "Jack M"],
+    minCounselors: 2,
+  },
+];
+counselors = [
+  { name: "Mason", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Grace H", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Asher", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Kyley", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Ryan", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Lily T", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Jack S", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Kara", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Jack H", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Becca", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Luke", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Grace K", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Jack M", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Lily K", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Mike", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Vanessa", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Justin", mon: "?", tues: "?", weds: "?", thurs: "?" },
+  { name: "Amy", mon: "?", tues: "?", weds: "?", thurs: "?" },
+];
+function determineDaysOff() {
+  // No idea
+}
+determineDaysOff()
