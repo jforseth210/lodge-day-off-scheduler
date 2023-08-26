fa5dd3bed3cf005885c24e46a69c7c414d079247
Justin Forseth <jforseth210@gmail.com>
2023-08-25 20:01:50 -0600

    Use chatgpt and tons of runtime debugging to translate brain.py to js

--- brain.js
+++ brain.js
@@ -1,156 +1,238 @@
-groups = [
-  {
-    name: "Mon Prayer",
-    days: ["Mon"],
-    counselors: ["Lily K"],
-    minCounselors: 1,
-  },
-  {
-    name: "Tues Prayer",
-    days: ["Tue"],
-    counselors: ["Justin"],
-    minCounselors: 1,
-  },
-  {
-    name: "Wed Prayer",
-    days: ["Wed"],
-    counselors: ["Kara"],
-    minCounselors: 1,
-  },
-  {
-    name: "Thurs Prayer",
-    days: ["Thurs"],
-    counselors: ["Luke"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 1",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Mason", "Grace H"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 2",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Kyley", "Asher"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 3",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Ryan", "Lily T"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 4",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Jack S", "Kara"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 5",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Jack H", "Becca"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 6",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Luke", "Grace K"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 7",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Jack M", "Lily K"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 8",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Mike", "Vanessa"],
-    minCounselors: 1,
-  },
-  {
-    name: "SG 9",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Justin", "Amy"],
-    minCounselors: 1,
-  },
-  {
-    name: "Bear",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Kara", "Kyley", "Amy", "Becca", "Grace H"],
-    minCounselors: 2,
-  },
-  {
-    name: "Wildcat",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Grace K", "Vanessa", "Lily K", "Lily T"],
-    minCounselors: 2,
-  },
-  {
-    name: "Coyote",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Mason", "Jack S", "Mike"],
-    minCounselors: 2,
-  },
-  {
-    name: "Goat's Nest",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Asher", "Jack H", "Justin"],
-    minCounselors: 2,
-  },
-  {
-    name: "Chipmunk",
-    days: ["Mon", "Tue", "Wed", "Thur"],
-    counselors: ["Ryan", "Luke", "Jack M"],
-    minCounselors: 2,
-  },
-];
-counselors = [
-  { name: "Mason", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Grace H", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Asher", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Kyley", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Ryan", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Lily T", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Jack S", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Kara", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Jack H", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Becca", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Luke", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Grace K", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Jack M", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Lily K", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Mike", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Vanessa", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Justin", mon: false, tues: false, weds: false, thurs: false },
-  { name: "Amy", mon: false, tues: false, weds: false, thurs: false },
-];
-function determineDaysOff() {
-  for (let i = 0; i < days.length; i++) {
-    const day = days[i];
-    for (let j = 0; j < groups.length; j++) {
-      const group = groups[j];
-      if (!hasEnough(group, day)) {
-        // Something something recursion
+const weekday = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY"];
+class Group {
+  constructor(name, days, minCounselors) {
+    this.name = name;
+    this.days = days;
+    this.minCounselors = minCounselors;
+    this.memberCounselors = [];
+  }
+
+  hasMinimum(day) {
+    // If the group doesn't apply to the given day,
+    // it doesn't need any counselors. For example, if it's Tuesday,
+    // and we want to know if the Monday session group has
+    // enough people, the answer is yes.
+    if (!this.days.includes(day)) {
+      return true;
+    }
+
+    let count = 0;
+    for (const counselor of this.memberCounselors) {
+      if (counselor.isOn(day)) {
+        count++;
+      }
+    }
+    return count >= this.minCounselors;
+  }
+
+  getCounselorsOff(day) {
+    return this.memberCounselors.filter((counselor) => !counselor.isOn(day));
+  }
+  getCounselorsOn(day) {
+    return this.memberCounselors.filter((counselor) => counselor.isOn(day));
+  }
+  getCounselorRatio(day) {
+    return this.minCounselors / this.getCounselorsOff(day).length;
+  }
+
+  addCounselor(counselor) {
+    this.memberCounselors.push(counselor);
+  }
+}
+class Counselor {
+  constructor(name) {
+    this.name = name;
+    this.daysOn = {
+      MONDAY: false,
+      TUESDAY: false,
+      WEDNESDAY: false,
+      THURSDAY: false,
+    };
+    this.groups = [];
+  }
+
+  addGroup(group) {
+    this.groups.push(group);
+  }
+
+  isOn(day) {
+    return this.daysOn[day];
+  }
+
+  setOn(day, isOn) {
+    this.daysOn[day] = isOn;
+
+    // The counselor is working every day this week!
+    if (isOn && Object.values(this.daysOn).every((value) => value)) {
+      return false;
+    }
+    return true;
+  }
+
+  canWorkAnotherDay() {
+    if (this.name === "Volunteer") {
+      return true;
+    }
+    let count = 0;
+    for (const day in this.daysOn) {
+      if (!this.isOn(day)) {
+        count++;
+      }
+    }
+    return count > 1;
+  }
+
+  getDaysOff() {
+    const off = [];
+    for (const [day, isOn] of Object.entries(this.daysOn)) {
+      if (!isOn) {
+        off.push(day);
+      }
+    }
+    return off;
+  }
+
+  setAllDaysOff() {
+    for (const day in this.daysOn) {
+      this.daysOn[day] = false;
+    }
+  }
+}
+
+const getRandomElement = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+function solve(groups, counselors) {
+  for (const day of weekday) {
+    for (const group of groups) {
+      while (!group.hasMinimum(day)) {
+        const counselorsOff = group.getCounselorsOff(day);
+        if (counselorsOff.length > 0) {
+          let addedSomeone = false;
+          for (const counselor of counselorsOff) {
+            if (counselor.canWorkAnotherDay()) {
+              counselor.setOn(day, true);
+              addedSomeone = true;
+              break;
+            }
+          }
+          if (!addedSomeone) {
+            console.log(`Not enough counselors for ${group.name} on ${day}`);
+            return false;
+          }
+        } else {
+          console.log(`Not enough counselors for ${group.name} on ${day}`);
+          return false;
+        }
+      }
+      console.log(`Solved: ${day}`)
+    }
+    groups.sort((a, b) => a.getCounselorRatio(day) - b.getCounselorRatio(day));
+  }
+
+  for (const day in weekday) {
+    if (counselors.every((counselor) => counselor.isOn(day))) {
+      console.log(`No one off on ${day}`);
+      return false;
+    }
+  }
+
+  for (const counselor of counselors) {
+    while (counselor.getDaysOff().length > 1) {
+      counselor.setOn(getRandomElement(counselor.getDaysOff()), true);
+    }
+  }
+
+  const result = [];
+  for (const counselor of counselors) {
+    result.push([counselor.name, counselor.getDaysOff()[0]]);
+  }
+  return result;
+}
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+async function fetchGroupsJson() {
+  const response = await fetch("groups.json");
+  const groupsJson = await response.json();
+  return groupsJson;
+}
+function addCounselorToGroup(counselor, group) {
+  counselor.addGroup(group);
+  group.addCounselor(counselor);
+}
+(async () => {
+  const groupsJson = await fetchGroupsJson();
+  const groups = [];
+  const counselors = [];
+
+  for (const groupJson of groupsJson) {
+    const group = new Group(
+      groupJson.name,
+      groupJson.days,
+      groupJson.minCounselors
+    );
+
+    for (const counselorName of groupJson.counselors) {
+      let counselor = counselors.find(
+        (counselor) => counselor.name === counselorName
+      );
+
+      if (!counselor) {
+        counselor = new Counselor(counselorName);
+        counselors.push(counselor);
+      }
+
+      addCounselorToGroup(counselor, group);
+    }
+
+    groups.push(group);
+  }
+
+  const TRIES = 10; // Change this to the desired number of tries
+  const solutions = [];
+
+  for (let i = 0; i < TRIES; i++) {
+    shuffle(counselors);
+    shuffle(groups);
+
+    for (const counselor of counselors) {
+      counselor.setAllDaysOff();
+    }
+
+    const solution = solve(groups, counselors);
+
+    if (solution) {
+      const sortedSolution = solution.map(([name, daysOn]) => [name, daysOn]);
+      solutions.push(sortedSolution);
+    }
+  }
+
+  // Deduplicate solutions
+  const uniqueSolutions = [...new Set(solutions.map(JSON.stringify))].map(
+    JSON.parse
+  );
+
+  for (const solution of uniqueSolutions) {
+    console.log("Possible days off:");
+    console.log("==================");
+
+    for (const [name, daysOn] of solution) {
+      console.log(name, daysOn);
+    }
+    for (const group of groups) {
+      console.log(group.name);
+      for (const day of weekday) {
+        console.log(
+          `   ${day} ${group
+            .getCounselorsOn(day)
+            .map((counselor) => counselor.name)
+            .join(",")}`
+        );
       }
     }
   }
-}
-function hasEnough(group, day) {
-  counter = 0;
-  for (let i = 0; i < group.counselors.length; i++) {
-    const counselor = group.counselors[i];
-    if (counselor[day]) {
-      counter++;
-    }
-    if (counter >= group.minCounselors) {
-      return true;
-    }
-  }
-  return false;
-}
-function availableCounselors() {}
-determineDaysOff();
+})();
