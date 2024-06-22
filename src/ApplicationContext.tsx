b9327b2eb6967187fc15012d4a8fd65375859fa5
Justin Forseth <justin@jforseth.com>
2024-06-22 15:14:09 -0600

    Add scheduleGenerated flag

--- src/ApplicationContext.tsx
+++ src/ApplicationContext.tsx
@@ -15,7 +15,6 @@ interface SerializableApplicationStateInterface {
   counselors: Counselor[];
   groups: Group[];
   relationships: Array<{ counselor: string; group: string }>;
-  scheduleGenerated: boolean;
 }
 export const ApplicationContext = createContext<ApplicationStateInterface>({
   counselors: [],
@@ -185,7 +184,6 @@ function saveState(state: ApplicationStateInterface) {
     counselors: [],
     groups: [],
     relationships: [],
-    scheduleGenerated: state.scheduleGenerated,
   };
   for (const counselor of state.counselors) {
     stateCopy.counselors.push(counselor.clone());
@@ -266,7 +264,7 @@ function loadState(): ApplicationStateInterface {
     return {
       counselors: counselors,
       groups: groups,
-      scheduleGenerated: parsedState.scheduleGenerated,
+      scheduleGenerated: false,
     };
   }
   return { counselors: [], groups: [], scheduleGenerated: false };
